8bccc9828346421415cc594ab90c3a61a3ca49dd
Kashif Zaidi <zkashif701@gmail.com>
2024-05-18 09:40:09 +0530

    Add files via upload

---
+++ stack_4_nearest_smaller_to_right.cpp
@@ -0,0 +1,44 @@
+//Stack - Nearest smaller element on right side of array
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int> vec;
+    stack<int> st;
+    for(int i=n-1;i>=0; i--){
+        if(st.size()==0){
+            vec.push_back(-1);
+        }
+        else if(st.size()>0 && arr[i] > st.top()){
+            vec.push_back(st.top());
+        }
+        else if(st.size()>0 && arr[i] <= st.top()){
+            while(st.size()>0 && arr[i] <= st.top()){
+                st.pop();
+            }
+            if(st.size()==0){
+                vec.push_back(-1);
+            }
+            else{
+                vec.push_back(st.top());
+            }
+        }
+        st.push(arr[i]);
+    }
+    reverse(vec.begin(), vec.end());
+    for(auto it: vec){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
