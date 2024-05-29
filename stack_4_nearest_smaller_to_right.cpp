4765e36f896d323387fc3fc9ef1b2dbd54eb198e
Kashif Zaidi <kzaidi337@gmail.com>
2024-05-29 15:24:54 +0530

    Update stack_4_nearest_smaller_to_right.cpp

--- stack_4_nearest_smaller_to_right.cpp
+++ stack_4_nearest_smaller_to_right.cpp
@@ -1,44 +1,39 @@
-//Stack - Nearest smaller element on right side of array
 #include <iostream>
-#include <bits/stdc++.h>
-#include <algorithm>
+#include <vector>
+#include <stack>
+#include <algorithm> // For reverse
+
 using namespace std;
 
-int main(){
-    int n;
-    cout<<"Enter the size of array: ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the elements of array: ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    vector<int> vec;
-    stack<int> st;
-    for(int i=n-1;i>=0; i--){
-        if(st.size()==0){
-            vec.push_back(-1);
-        }
-        else if(st.size()>0 && arr[i] > st.top()){
-            vec.push_back(st.top());
-        }
-        else if(st.size()>0 && arr[i] <= st.top()){
-            while(st.size()>0 && arr[i] <= st.top()){
+class Solution {
+public:
+    // Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n) {
+        stack<long long> st;
+        vector<long long> result(n);
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && arr[i] >= st.top()) {
                 st.pop();
             }
-            if(st.size()==0){
-                vec.push_back(-1);
+            if(st.empty()) {
+                result[i] = -1;
+            } else {
+                result[i] = st.top();
             }
-            else{
-                vec.push_back(st.top());
-            }
+            st.push(arr[i]);
         }
-        st.push(arr[i]);
+        return result;
     }
-    reverse(vec.begin(), vec.end());
-    for(auto it: vec){
-        cout<<it<<" ";
+};
+
+int main() {
+    Solution sol;
+    vector<long long> arr = {1, 3, 2, 4};
+    vector<long long> result = sol.nextLargerElement(arr, arr.size());
+
+    for(long long x : result) {
+        cout << x << " ";
     }
-    cout<<endl;
+
     return 0;
 }
