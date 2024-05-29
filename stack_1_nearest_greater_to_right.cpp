93778ac154dc7e53525674982c0b1b1cc22c1e20
Kashif Zaidi <kzaidi337@gmail.com>
2024-05-29 15:29:29 +0530

    Update stack_1_nearest_greater_to_right.cpp

--- stack_1_nearest_greater_to_right.cpp
+++ stack_1_nearest_greater_to_right.cpp
@@ -1,76 +1,39 @@
-//Stack - Nearest greater element on right side of array
-//Brute force
 #include <iostream>
-#include <bits/stdc++.h>
-#include <algorithm>
+#include <vector>
+#include <stack>
+#include <algorithm> // For reverse
+
 using namespace std;
-// int main() {
-//     int n;
-//     cout<<"Enter the size of array: ";
-//     cin>>n;
-//     cout<<"Enter the elements of array: ";
-//     int arr[n];
-//     for(int i=0; i<n; i++){
-//         cin>>arr[i];
-//         cout<<" ";
-//     }
-//     int arr1[n];
-//     for (int p = 0; p < n; ++p) {
-//         arr1[p] = -1;
-//     }
 
-//     for(int k=0; k<n; k++){
-//         for(int l=k+1; l<n; l++){
-//             if(arr[k]<arr[l]){
-//                 arr1[k] = arr[l];
-//                 break; 
-//             }
-//         }
-//     }
-//     for(int j=0; j<n; j++){
-//         cout<<arr1[j]<<" ";
-//     }
-//     cout<<endl;
-//     return 0;
-// }
-
-// stack implementation
-
-int main(){
-    int n;
-    cout<<"Enter the size of array: ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the elements of array: ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    vector<int> vec;
-    stack<int> st;
-    for(int j=n-1;j>-1;j--){
-        if(st.size()==0){
-            vec.push_back(-1);
-        }
-        else if(st.size()>0 && arr[j]<st.top()){
-            vec.push_back(st.top());
-        }
-        else if(st.size()>0 && arr[j]>=st.top()){
-            while(st.size()>0 && arr[j]>=st.top()){
+class Solution {
+public:
+    // Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n) {
+        stack<long long> st;
+        vector<long long> result(n);
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && arr[i] >= st.top()) {
                 st.pop();
             }
-            if(st.size()==0){
-                vec.push_back(-1);
+            if(st.empty()) {
+                result[i] = -1;
+            } else {
+                result[i] = st.top();
             }
-            else{
-                vec.push_back(st.top());
-            }
+            st.push(arr[i]);
         }
-        st.push(arr[j]);
+        return result;
     }
-    reverse(vec.begin(), vec.end());
-    for(auto it: vec){
-        cout<<it<<" ";
+};
+
+int main() {
+    Solution sol;
+    vector<long long> arr = {1, 3, 2, 4};
+    vector<long long> result = sol.nextLargerElement(arr, arr.size());
+
+    for(long long x : result) {
+        cout << x << " ";
     }
-    cout<<endl;
+
     return 0;
 }
